de675f1f52819c2d341b988de23c06ba564916a2
Eduardo Gonzalez <eduardo.gonzalezpacheco@kairosds.com>
2022-04-22 21:32:03 +0200

    feat: working aws presigned config

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,29 +1,62 @@
-import { S3, S3Client } from '@aws-sdk/client-s3';
+import { S3 } from '@aws-sdk/client-s3';
+import AWS from 'aws-sdk';
 import 'dotenv/config';
 
+const bucket = 'dashboard-react';
+const objectKey = 'edogp/Tron-legacy';
+const region = 'eu-west-3';
+
 const getFiles = async () => {
-  const bucket = 'dashboard-react';
   const s3Config = {
-    region: 'eu-west-3',
+    region,
     // region: 'us-east-1',
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
     },
   };
-  // const clientv2 = new s3v2();
   const client = new S3(s3Config);
-  const client2 = new S3Client(s3Config);
+
   try {
-    const files = await client.listObjectsV2({ Bucket: 'dashboard-react' });
+    const files = await client.listObjectsV2({ Bucket: bucket });
     console.log({ files: files.Contents });
+    const object = await client.getObject({
+      Bucket: bucket,
+      Key: objectKey,
+    });
+    const presignedUrl = await client.getObjectAcl({
+      Bucket: bucket,
+      Key: objectKey,
+    });
+    console.log({ object, presignedUrl });
     const bucketACL = await client.getBucketAcl({ Bucket: bucket });
-    const info = await client2.send;
     console.log(JSON.stringify({ bucketACL: bucketACL.Grants }, null, 2));
-    console.log(JSON.stringify({ info }, null, 2));
   } catch (error) {
     console.log(error);
   }
 };
 
-getFiles();
+const getFilesV2 = async () => {
+  AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+    region,
+    signatureVersion: 'v4',
+  });
+  const s3 = new AWS.S3();
+  const signedUrlExpireSeconds = 60 * 1; // 5 minutes
+
+  const url = s3.getSignedUrl('getObject', {
+    Bucket: bucket,
+    Key: objectKey,
+    Expires: signedUrlExpireSeconds,
+  });
+  console.log({ url });
+};
+
+getFilesV2();
+
+export default {
+  getFiles,
+  getFilesV2,
+};
